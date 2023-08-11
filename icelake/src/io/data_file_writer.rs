dba8799804b350f6afc891a9b5163783c4fe122b
Renjie Liu <liurenjie2008@gmail.com>
2023-08-11 13:03:41 +0800

    disable dictionalry

--- icelake/src/io/data_file_writer.rs
+++ icelake/src/io/data_file_writer.rs
@@ -125,7 +125,7 @@ impl DataFileWriter {
                 .set_data_page_row_count_limit(20 * 1000)
                 .set_dictionary_page_size_limit(2 * 1024 * 1024)
                 .set_compression(Compression::GZIP(GzipLevel::default()))
-                .set_dictionary_enabled(true)
+                .set_dictionary_enabled(false)
                 .build();
             ParquetWriterBuilder::new(file_writer, self.arrow_schema.clone())
                 .with_properties(props)
